1ec6b8000f1a2d74e0f9949b798b0a4d61aeba97
Mathieu <msmat@GPTOVC>
2024-02-10 16:56:32 +0100

    First commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function preload() {}
+
+function setup() {
+  let canvas = createCanvas(640, 480);
+
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+}
+
+function update() {}
+
+function draw() {
+  update();
+}
+
+function keyPressed() {
+  return false; // prevent any default behaviour
+}
